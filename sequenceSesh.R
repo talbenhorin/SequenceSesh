5a31b93a9ab1cd8f909cc7252b1873f31bdb668b
Tal Ben-Horin PhD <tbenhor@ncsu.edu>
2023-02-22 16:10:36 -0500

    Update

--- sequenceSesh.R
+++ sequenceSesh.R
@@ -28,13 +28,56 @@ filterAndTrim(fwd=file.path(pathF, fastqFs), filt=file.path(filtpathF, fastqFs),
 # Infer sequence variants
 # File parsing
 
-filtpathF <- "FNB/filtered" # CHANGE ME to the directory containing your filtered forward fastqs
+filtpathF <- "FNB/filtered" 
 filtpathR <- "RNB/filtered"
 filtFs <- list.files(filtpathF, pattern="fastq.gz", full.names = TRUE)
 filtRs <- list.files(filtpathR, pattern="fastq.gz", full.names = TRUE)
+
 sample.names <- sapply(strsplit(basename(filtFs), "_"), `[`, 1) # Assumes filename = samplename_XXX.fastq.gz
 sample.namesR <- sapply(strsplit(basename(filtRs), "_"), `[`, 1) # Assumes filename = samplename_XXX.fastq.gz
+
 if(!identical(sample.names, sample.namesR)) stop("Forward and reverse files do not match.")
+
 names(filtFs) <- sample.names
 names(filtRs) <- sample.names
 
+set.seed(100)
+# Learn forward error rates
+errF <- learnErrors(filtFs, nbases=1e8, multithread=TRUE)
+# Learn reverse error rates
+errR <- learnErrors(filtRs, nbases=1e8, multithread=TRUE)
+
+# Sample inference and merger of paired-end reads
+mergers <- vector("list", length(sample.names))
+names(mergers) <- sample.names
+
+for(sam in sample.names) {
+  cat("Processing:", sam, "\n")
+  derepF <- derepFastq(filtFs[[sam]])
+  ddF <- dada(derepF, err=errF, multithread=TRUE)
+  derepR <- derepFastq(filtRs[[sam]])
+  ddR <- dada(derepR, err=errR, multithread=TRUE)
+  merger <- mergePairs(ddF, derepF, ddR, derepR)
+  mergers[[sam]] <- merger
+}
+rm(derepF); rm(derepR)
+
+# Construct sequence table
+seqtab <- makeSequenceTable(mergers)
+saveRDS(seqtab, "E:/tbenhor/libraries/Documents/SequenceSesh/Output/seqtab.rds") # CHANGE ME to where you want sequence table saved
+
+# Remove chimeras
+st.all <- readRDS("E:/tbenhor/libraries/Documents/SequenceSesh/Output/seqtab.rds")
+seqtab <- removeBimeraDenovo(st.all, method="consensus", multithread=TRUE)
+
+# Assign taxonomy
+tax <- assignTaxonomy(seqtab, "E:/tbenhor/libraries/Documents/SequenceSesh/silva_nr_v132_train_set.fa.gz", multithread=TRUE)#get tax
+tax <- addSpecies(tax, "E:/tbenhor/libraries/Documents/SequenceSesh/silva_species_assignment_v132.fa.gz")
+
+taxa.print <- tax # Removing sequence rownames for display only
+rownames(taxa.print) <- NULL
+head(taxa.print)
+
+## Save files
+saveRDS(seqtab, "E:/tbenhor/libraries/Documents/SequenceSesh/Output/seqtab_final.rds") 
+saveRDS(tax, "E:/tbenhor/libraries/Documents/SequenceSesh/Output/tax_final.rds") 
